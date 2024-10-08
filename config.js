a38f89a93d4aa54e76227bb9221ed90694358d58
ayush-23-30 <pawar85060@gmail.com>
2024-10-08 22:19:50 +0530

    update

--- config.js
+++ config.js
@@ -1,16 +1,17 @@
 import { configDotenv } from "dotenv";
 import mongoose from "mongoose";
 
-
 configDotenv(); 
-const ConnectionWithDb = async ()=>{
-    await mongoose.connect(`${process.env.MONGO_DB}/${process.env.MONGO_DB_NAME}`)
-    .then(()=>{
-      console.log("DB is connected Successfully");
-    }).catch((error)=>{
-      console.error("there is an error in the connection establishing with db");
-      process.exit(1); 
-    })
-}
 
-export default ConnectionWithDb; 
+const ConnectionWithDb = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_DB}/${process.env.MONGO_DB_NAME}`);
+    console.log("DB is connected Successfully");
+  } catch (error) {
+    console.error("Error connecting to the DB:", error.message);
+    process.exit(1);
+  }
+};
+
+
+export default ConnectionWithDb;
