20b0a25f5520ac4de5427d8a5c4b25d5848dca8d
ayush-23-30 <pawar85060@gmail.com>
2024-10-09 12:41:28 +0530

    db ip update

--- config.js
+++ config.js
@@ -1,17 +1,24 @@
-import { configDotenv } from "dotenv";
 import mongoose from "mongoose";
 
-configDotenv(); 
+let isConnected = false; // Global connection flag
 
 const ConnectionWithDb = async () => {
+  if (isConnected) {
+    console.log("Using existing database connection");
+    return;
+  }
   try {
-    await mongoose.connect(`${process.env.MONGO_DB}/${process.env.MONGO_DB_NAME}`);
-    console.log("DB is connected Successfully");
+    const db = await mongoose.connect(process.env.MONGO_DB, {
+     
+      serverSelectionTimeoutMS: 5000, // Timeout for server selection
+      connectTimeoutMS: 10000,        // Timeout for initial connection
+    });
+    isConnected = db.connections[0].readyState;
+    console.log("Database connected successfully");
   } catch (error) {
-    console.error("Error connecting to the DB:", error.message);
+    console.error("Failed to connect to the database", error.message);
     process.exit(1);
   }
 };
-
 
 export default ConnectionWithDb;
